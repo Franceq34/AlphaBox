507e0e96ace1bc727a5a568661ae86401d631682
Quentin FRANCE <quentin.france@free.fr>
2019-05-17 19:07:29 +0200

    SocketIO integration

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-
+const io = require('socket.io-client');
 const express = require('express');
 const app = express();
-const request = require('request')
+const request = require('request');
 const { getJumpsAlphaNumber } = require('./retrieveDataHarness');
 
 const url = "http://localhost:3333"
-const time = 3 * 60 * 1000
+const time = 10000
+
+const socket = io(url);
 
 function activeConnection() {
 	request.post(url + '/box/activeConnection', {
@@ -17,7 +19,8 @@ function activeConnection() {
 			console.error(error)
 			return
 		}
-		console.log(`statusCode: ${res.statusCode}`)
+		socket.emit('BOX_CONNECTED_TO_SERVER');
+		console.log(`BOX_CONNECTED_TO_SERVER : statusCode: ${res.statusCode}`)
 	})
 }
 
@@ -31,6 +34,8 @@ function sendDataHarness(dataHarness, callback) {
 			console.error(error)
 			return
 		}
+		socket.emit('NEW_JUMPS_ON_SERVER');
+		console.log(`NEW_JUMPS_ON_SERVER : statusCode: ${res.statusCode}`)
 		console.log(`Statut de l'envoi: ${res.statusCode}, message : ${res.statusMessage}`)
 	
 		callback()
@@ -48,23 +53,53 @@ function getAlphasFromFarmer(idFarm, callback) {
 		} 
 		callback(res.body)
 	})
-}
+} 
 
 function boucle() {
 	activeConnection()
-	// getAlphasFromFarmer(59, res => {
-	// 	console.log(res);
-	// });
-	let numAlpha = 8;
-	//let jumps = {data : [{"numHarness":8,"numRecord":0,"numEwe":200001617678,"date":"2020-02-08T00:55:50.000Z"}], idFarmer:"89"}
-	getJumpsAlphaNumber(numAlpha).then(jumps => {
-		let bodyToSent = {data : jumps, idFarmer:59};
-		sendDataHarness(bodyToSent, () => {
-			// Recommence indéfiniment
-
+	let bodyToSent = {
+		idFarmer: 59,
+		data: [{
+			numHarness: 19,
+			numEwe: 91,
+			date: "2020-09-15T15:53:00"
+		},
+		{
+			numHarness: 19,
+			numEwe: 92,
+			date: "2020-09-15T15:53:00"
+		},
+		{
+			numHarness: 19,
+			numEwe: 93,
+			date: "2020-09-15T15:53:00"
+		},
+		{
+			numHarness: 19,
+			numEwe: 92,
+			date: "2020-09-15T15:53:00"
+		},
+		{
+			numHarness: 19,
+			numEwe: 93,
+			date: "2020-09-15T15:53:00"
+		}]
+	}
+	sendDataHarness(bodyToSent, () => {
 			setTimeout(boucle, time);
 		});
-	})
+	 getAlphasFromFarmer(59, res => {
+	 	console.log(res);
+	});
+	let numAlpha = 8;
+	//let jumps = {data : [{"numHarness":8,"numRecord":0,"numEwe":200001617678,"date":"2020-02-08T00:55:50.000Z"}], idFarmer:"89"}
+	//getJumpsAlphaNumber(numAlpha).then(jumps => {
+	//	let bodyToSent = {data : jumps, idFarmer:59};
+	//	sendDataHarness(bodyToSent, () => {
+	//		// Recommence indéfiniment
+	//		setTimeout(boucle, time);
+	//	});
+	//})
 }
 boucle();
 
